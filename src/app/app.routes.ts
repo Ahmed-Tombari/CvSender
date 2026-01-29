d98f559f05272ab106cab271ae8f6ad60f397178
Ahmed-Tombari <tombariahmed9@mail.com>
2026-01-29 23:20:37 +0100

    fix routing spa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+  { path: '', loadComponent: () => import('./components/landing/landing').then(m => m.LandingComponent) },
   { path: 'portfolio', loadComponent: () => import('./components/portfolio/portfolio').then(m => m.PortfolioComponent) },
   { path: 'dashboard', loadComponent: () => import('./components/landing/landing').then(m => m.LandingComponent) },
   { path: 'apply/success', loadComponent: () => import('./components/success/success').then(m => m.ApplySuccessComponent) },
-  { path: '**', redirectTo: 'portfolio' }
+  { path: '**', redirectTo: '' }
 ];
